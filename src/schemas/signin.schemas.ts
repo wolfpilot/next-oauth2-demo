46a39f055b077c163cea63575a7f34e0c44d4c61
Razvan Negrea <rznart@gmail.com>
2024-11-02 10:41:53 +0100

    Update Zod schemas to trim input values before validation

--- src/schemas/signin.schemas.ts
+++ src/schemas/signin.schemas.ts
@@ -8,11 +8,13 @@ export const signinSchema = z.object({
     .string({
       required_error: "Email is required.",
     })
+    .trim()
     .email({ message: "Invalid email address." }),
   password: z
     .string({
       required_error: "Password is required.",
     })
+    .trim()
     .min(8, { message: "Password must be at least 8 characters long." })
     .regex(/[a-zA-Z]/, {
       message: "Password must contain at least one letter.",
--- src/schemas/signup.schemas.ts
+++ src/schemas/signup.schemas.ts
@@ -8,6 +8,7 @@ export const signupSchema = z.object({
     .string({
       required_error: "Name is required.",
     })
+    .trim()
     .min(2, {
       message: "Name must be at least 2 characters long.",
     }),
@@ -15,11 +16,13 @@ export const signupSchema = z.object({
     .string({
       required_error: "Email is required.",
     })
+    .trim()
     .email({ message: "Invalid email address." }),
   password: z
     .string({
       required_error: "Password is required.",
     })
+    .trim()
     .min(8, { message: "Password must be at least 8 characters long." })
     .regex(/[a-zA-Z]/, {
       message: "Password must contain at least one letter.",
@@ -27,8 +30,7 @@ export const signupSchema = z.object({
     .regex(/[0-9]/, { message: "Password must contain at least one number." })
     .regex(/[^a-zA-Z0-9]/, {
       message: "Password must contain at least one special character.",
-    })
-    .trim(),
+    }),
 })
 
 export type SignupSchema = z.infer<typeof signupSchema>
